53302b75b3b8df5fd5674f673d67ae2086d97055
john pryke <jnpryke@gmail.com>
2021-02-09 02:50:48 -0500

    Unsubscribing from weather data

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import IWeatherResponse from '../models/weather-model';
 import { DataService } from '../services/data.service';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-temperature',
   templateUrl: './temperature.component.html',
   styleUrls: ['./temperature.component.scss']
 })
-export class TemperatureComponent implements OnInit {
+export class TemperatureComponent implements OnInit, OnDestroy {
   public temperature: number = null
+  private isAlive: boolean = true;
 
   constructor(private dataService: DataService) { }
 
   public ngOnInit(): void {
-    this.dataService.getWeather().subscribe((res: IWeatherResponse) => {
-      this.temperature = res.main.temp;
-    });
+    this.dataService.getWeather()
+      .pipe(takeWhile((_) => this.isAlive))
+      .subscribe((res: IWeatherResponse) => {
+        this.temperature = res.main.temp;
+      });
   }
+
+  public ngOnDestroy(): void {
+    this.isAlive = false;
+  }
+
 }
